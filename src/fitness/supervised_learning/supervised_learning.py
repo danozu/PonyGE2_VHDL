bbe357ed715b74f00597fce4e5b7fcb5979b3926
danozu <allan.danilo1987@gmail.com>
2021-06-08 21:09:34 -0300

    Update supervised_learning.py

--- src/fitness/supervised_learning/supervised_learning.py
+++ src/fitness/supervised_learning/supervised_learning.py
@@ -17,7 +17,6 @@ import shutil
 
 from stats.stats import stats
 
-
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True, cwd=r'../../VHDL/individuals')
     proc_stdout = process.communicate()[0].strip()
