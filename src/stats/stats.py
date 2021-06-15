ddff4cdd5850b1de135a4a55ff97d52f7f7da176
danozu <allan.danilo1987@gmail.com>
2021-06-15 18:16:33 -0300

    1

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -112,7 +112,7 @@ def get_soo_stats(individuals, end):
     elif not params['SILENT']:
         # Print simple display output.
         perc = stats['gen'] / (params['GENERATIONS']+1) * 100
-        stdout.write("Evolution: %d%% complete\r" % perc)
+        stdout.write("=======================================\n\nEvolution: %d%% complete\r" % perc)
         stdout.flush()
 
     # Generate test fitness on regression problems
@@ -238,7 +238,7 @@ def get_moo_stats(individuals, end):
     elif not params['SILENT']:
         # Print simple display output.
         perc = stats['gen'] / (params['GENERATIONS'] + 1) * 100
-        stdout.write("Evolution: %d%% complete\r" % perc)
+        stdout.write("=======================================\n\nEvolution: %d%% complete\r" % perc)
         stdout.flush()
 
     # Generate test fitness on regression problems
